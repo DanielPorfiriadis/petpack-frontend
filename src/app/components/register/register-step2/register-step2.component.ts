4fd2717211d1a4f61a9b318e31f4c40a80aebd30
Euaggelia Pro <giliapro_93@hotmail.gr>
2020-10-20 14:52:41 +0300

    .

---
+++ src/app/components/register/register-step2/register-step2.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-step2',
+  templateUrl: './register-step2.component.html',
+  styleUrls: ['./register-step2.component.css']
+})
+export class RegisterStep2Component implements OnInit {
+
+  constructor() { }
+  @Input() regForm: FormGroup;
+
+  ngOnInit() {
+  }
+
+  step2Submitted() {
+    this.regForm.get('petDetails').get('petName').markAsTouched();
+    this.regForm.get('petDetails').get('petName').updateValueAndValidity();
+    this.regForm.get('petDetails').get('species').markAsTouched();
+    this.regForm.get('petDetails').get('species').updateValueAndValidity();
+    this.regForm.get('petDetails').get('gender').markAsTouched();
+    this.regForm.get('petDetails').get('gender').updateValueAndValidity();
+  }
+
+}
---
+++ src/app/components/register/register-step3/register-step3.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-step3',
+  templateUrl: './register-step3.component.html',
+  styleUrls: ['./register-step3.component.css']
+})
+export class RegisterStep3Component implements OnInit {
+
+  constructor() { }
+
+  @Input() regForm: FormGroup;
+  formSubmitted: boolean = false;
+
+  ngOnInit() {
+  }
+
+  submit() {
+    console.log('submitted');
+    console.log(this.regForm.value);
+    this.formSubmitted = true;
+
+  }
+
+}
