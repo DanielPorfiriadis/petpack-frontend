04298094f1fcceb64282444a49907f5cdc73447a
Euaggelia Pro <giliapro_93@hotmail.gr>
2020-10-30 18:14:13 +0200

    new commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,35 +6,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-/* Feed*/
-import { FeedFormComponent } from './components/feed-form/feed-form.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FeedComponent } from './components/feed/feed.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'landing-page' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'landing-page', component: LandingPageComponent },
-
-  /* Feed*/
-
-  { path: 'new', component: FeedFormComponent },
-  {
-    path: 'feed',
-    component: FeedComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full',
-  },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '**', component: PageNotFoundComponent },
-
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-=======
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
->>>>>>> 5369d94190eebcae466a8dd586c8c3c8789b850f
 
 /* Routing */
 import { AppRoutingModule } from './app-routing.module';
-/* new import for feed*/ import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
 
 /* Angular Material */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,7 +17,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 /* Components */
-import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component'
 import { LoginComponent } from './components/login/login.component';
 import { RegisterStep1Component } from './components/register/register-step1/register-step1.component';
@@ -29,62 +25,29 @@ import { RegisterStep3Component } from './components/register/register-step3/reg
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-/* Feed*/
-import { FeedFormComponent } from './components/feed-form/feed-form.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { FeedComponent } from './components/feed/feed.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 
-
-/* Feed stop */
-
-/*Angular Material*/
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './angular-material.module';
-
-/*Steps*/
-import { RegistrationStep1Component } from './components/registration-step1/registration-step1.component';
-import { RegistrationStep2Component } from './components/registration-step2/registration-step2.component';
-import { RegistrationStep3Component } from './components/registration-step3/registration-step3.component';
-/*apo edw: https://github.com/srikanthmadasu/angular-material-stepper-example/tree/master/src/app/components/registration-step1*/
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
     RegisterComponent,
     RegisterStep1Component,
     RegisterStep2Component,
     RegisterStep3Component,
     LandingPageComponent,
-    FeedFormComponent,
-    DashboardComponent,
-    FeedComponent,
-    PageNotFoundComponent,
-=======
-    RegistrationStep1Component,
-    RegistrationStep2Component,
-    RegistrationStep3Component
->>>>>>> 5369d94190eebcae466a8dd586c8c3c8789b850f
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
     AngularMaterialModule,
     ReactiveFormsModule,
     FormsModule,
     FlexLayoutModule,
-    HttpClientModule,
-    
-=======
-    AngularMaterialModule
->>>>>>> 5369d94190eebcae466a8dd586c8c3c8789b850f
+    HttpClientModule
   ],
-
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
