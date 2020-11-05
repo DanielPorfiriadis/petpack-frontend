5e6066a1edf7dafae7bce37d85eede4b1a37d41d
Euaggelia Pro <giliapro_93@hotmail.gr>
2020-11-05 20:33:26 +0200

    latest commit

---
+++ src/app/components/register/register-step3/register-step3.component_LOCAL_1356.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Service } from '../../services';
+
+@Component({
+  selector: 'app-register-step3',
+  templateUrl: './register-step3.component.html',
+  styleUrls: ['./register-step3.component.css']
+})
+export class RegisterStep3Component implements OnInit {
+
+  constructor(public service: Service) { }
+
+  @Input() regForm: FormGroup;
+  @Input() persDetails;
+  @Input() petsDetails;
+  formSubmitted: boolean = false;
+
+  ngOnInit() {
+  }
+
+  submit() {
+    /*console.log('submitted');
+    console.log(this.regForm.value);*/
+    this.formSubmitted = true;
+
+    console.log(this.regForm.value);
+    const respondMessages = this.service.registerService(this.regForm).subscribe();
+    console.log(respondMessages);
+
+  }
+
+}
+
+
