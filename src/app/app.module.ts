bc56be30301ba1201c75fa1982420741324f25bb
Gpapadoa <gpapadoa@gmail.com>
2020-11-09 15:16:38 +0200

    t2t

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 /* Routing */
 import { AppRoutingModule } from './app-routing.module';
-
+/* new import for feed*/ import { RouterModule, Routes } from '@angular/router';
 
 /* Angular Material */
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 /* FormsModule */
-//import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 /* Angular Flex Layout */
-//import { FlexLayoutModule } from "@angular/flex-layout";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 /* Components */
 import { AppComponent } from './app.component';
@@ -23,28 +23,20 @@ import { RegisterStep1Component } from './components/register/register-step1/reg
 import { RegisterStep2Component } from './components/register/register-step2/register-step2.component';
 import { RegisterStep3Component } from './components/register/register-step3/register-step3.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
-
-=======
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FirstNavbarComponent } from './components/first-navbar/first-navbar.component';
->>>>>>> origin/vasilis_development
 
 /*npm install ngx - cookie - service--save*/
 import { CookieService } from 'ngx-cookie-service'; 
 
 /* Feed*/
 import { AfterloginComponent } from './components/afterlogin/afterlogin.component';
-
 import { FeedPageComponent } from './components/feed-page/feed-page.component'
 import { NavbarComponent } from './components/feed-page/navbar/navbar.component';
 import { FeedviewComponent } from './components/feed-page/feedview/feedview.component';
 
-
-
 /* npm install angular-countdown-date-time */
-//import { AngularCountdownDateTimeModule } from 'angular-countdown-date-time';
+import { AngularCountdownDateTimeModule } from 'angular-countdown-date-time';
 
 
 
@@ -66,13 +58,13 @@ import { FeedviewComponent } from './components/feed-page/feedview/feedview.comp
   imports: [
     BrowserModule,
     AppRoutingModule,
-    //BrowserAnimationsModule,
+    BrowserAnimationsModule,
     AngularMaterialModule,
-    //ReactiveFormsModule,
-    //FormsModule,
-    //FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
     HttpClientModule,
-    //AngularCountdownDateTimeModule,
+    AngularCountdownDateTimeModule,
   ],
 
   providers: [ CookieService ],
