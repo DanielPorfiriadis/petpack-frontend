5a85585a595a7d2d683eb25ce5d05b53396ab5f7
VasilisMerk <basilismerko123@gmail.com>
2020-11-09 14:45:48 +0200

    Fixes + Navbar apo Gilia

--- src/app/components/feed-page/navbar/navbar.component.ts
+++ src/app/components/feed-page/navbar/navbar.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+
+// import { AuthService } from 'auth';
+
+
+/*import { LoggingService, Config } from 'loggerservice';*/
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  @Output() toggleSidenav = new EventEmitter<void>();
+
+  private returnUrl = '/';
+
+  constructor(
+              private router: Router,
+              ) {
+
+  this.router.events.subscribe((event) => {
+
+    if (event instanceof NavigationEnd) {
+
+      this.returnUrl = event.url;
+
+      // this.logger.info('NavbarComponent returnUrl: ' + this.returnUrl);
+    }
+
+  });
+
+}
+
+public onProfile() {
+
+  this.router.navigate(['users/profile']);
+}
+
+public logout() {
+
+  // this.authService.logout(this.returnUrl || '/');
+}
+
+public settings(){
+  // this.settings.....kati
+}
 
   ngOnInit(): void {
   }
+
+  display = false;
+  onPress() {
+    this.display = true;
+}
 
 }
