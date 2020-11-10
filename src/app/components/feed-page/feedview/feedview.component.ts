2cf605aa8fdf7deee97d7279c21bf15a9602084b
VasilisMerk <basilismerko123@gmail.com>
2020-11-10 11:19:56 +0200

    Upload Image

--- src/app/components/feed-page/feedview/feedview.component.ts
+++ src/app/components/feed-page/feedview/feedview.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-feedview',
   templateUrl: './feedview.component.html',
@@ -9,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedviewComponent implements OnInit {
 
-  message: string;
-
+  message: any;
+  image: any;
 
   items: number[] = [];
   posts: any[] = [];
+
+  file: File;
+  fileName: string;
+
+  url;
+  urls: any[]=[];
+  msg = "";
+
+
   constructor() {
     for (let i = 0; i < 5; i++) {
       this.items.push(i);
@@ -24,12 +32,55 @@ export class FeedviewComponent implements OnInit {
    
   }
 
+  aButton(eikona): void {
+    this.posts.push(eikona);
+  }
+
   createNewPost(): void {
-    this.posts.push(this.message);
+    if (this.message) { this.posts.push(this.message); }/* else if(this.url) {
+      this.posts.push('');
+    }*/
+    if (this.url) {
+      this.urls.push(this.url);
+      /*this.posts.push(this.url)*/
+    } else {
+      this.urls.push('');
+    }
+    this.posts.reverse();
+    this.urls.reverse();
   }
 
   deletePost(i: number): void {
-    this.posts.splice(i,1)
+    this.posts.splice(i,1);
   }
+
+  fileUpload(file): void {
+    this.file = file.files[0];
+    this.fileName = file.files[0].name;
+  }
+
+  selectFile(event) {
+    if (!event.target.files[0] || event.target.files[0].length == 0) {
+      this.msg = 'You must select an image';
+      return;
+    }
+
+    var mimeType = event.target.files[0].type;
+
+    if (mimeType.match(/image\/*/) == null) {
+      this.msg = "Only images are supported";
+      return;
+    }
+
+    var reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+
+    reader.onload = (_event) => {
+      this.msg = "";
+      this.url = reader.result;
+      
+    }
+  }
+
 
 }
