a081b0990b17c54e1696d3fcb241a8774ac2144d
VasilisMerk <basilismerko123@gmail.com>
2020-11-17 13:37:28 +0200

    Merge from pap

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
