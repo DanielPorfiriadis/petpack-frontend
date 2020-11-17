003d1e1ded0201eef67cfb1bda5e1d7296f3e964
Euaggelia Pro <giliapro_93@hotmail.gr>
2020-11-17 12:27:33 +0200

    merge from pap

--- src/app/components/auth/pet.service.ts
+++ src/app/components/auth/pet.service.ts
@@ -1,18 +1,61 @@
-// import { Injectable } from "@angular/core";
-// import { HttpClient } from "@angular/common/http";
-// import { Router } from "@angular/router";
-// import { Subject } from "rxjs";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { Subject } from "rxjs";
 
-// import { PetData } from "./pet.model";
+import { PetData } from "./pet.model";
+import { map } from 'rxjs/operators';
 
-// export interface Pet {
-//     petName: string;
-//     species: string;
-//     gender: string;
-// }
+@Injectable({ providedIn: "root" })
+export class PetService {
+    constructor(private http: HttpClient, private router: Router) {}
 
-// @Injectable({ providedIn: "root" })
-// export class PetService {
+    private pets: PetData[]=[];
+    private petsCount: number;
+    private petsUpdated = new Subject<{ pets: PetData[]; petsCount: number }>();
+
+    createPet(petName: string, species: string, gender: string, ownerUsername: string) {
+        const petData: PetData = { petName: petName, species: species, gender: gender, ownerUsername: null, id:null};
+        console.log(petData);
+        this.http
+          .post("http://localhost:3000/api/pet/add/"+ownerUsername, petData).subscribe(res =>{
+              console.log(res);
+          });
+    }
+    getUserPets(username: string){
+        this.http.get<{ message: string; pets: any; petsCount: number; }>(
+            "http://localhost:3000/api/pet/get/" + username )
+            .pipe(
+                map(petData=> {
+                    return {
+                        pets: petData.pets.map(pet => {
+                            return{
+                                petName: pet.petName,
+                                id: pet._id,
+                                ownerUsername: pet.ownerUsername,
+                                gender: pet.gender,
+                                species: pet.species
+                            };
+                        }),
+                        totalPets: petData.petsCount
+                    };
+                })            
+            )
+            .subscribe(transformedPetData =>{
+                
+                this.pets = transformedPetData.pets;
+                console.log(this.pets);
+                this.petsUpdated.next({
+                    pets: [...this.pets],
+                    petsCount: transformedPetData.totalPets
+                });
+            });
+    }
+    getPetsUpdateListener(){
+        return this.petsUpdated.asObservable();
+    }
+}
+  
 //   private isAuthenticated = false;
 //   private token: string;
 //   private userId: string;
--- src/app/components/auth/register/register-step3/register-step3.component.ts
+++ src/app/components/auth/register/register-step3/register-step3.component.ts
@@ -1,30 +1,62 @@
 import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
-import { Form, FormGroup } from '@angular/forms';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../auth.service';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { PetService } from '../../pet.service';
+import { mimeType } from "../../../feed-page/feedview/mime-type.validator";
 
 @Component({
   selector: 'app-register-step3',
   templateUrl: './register-step3.component.html',
   styleUrls: ['./register-step3.component.css']
 })
-export class RegisterStep3Component {
+export class RegisterStep3Component implements OnInit{
 
-  constructor(public authService: AuthService, private router: Router) { }
+  constructor(public authService: AuthService, private router: Router, public petService: PetService) { }
 
   @Input() regForm: FormGroup;
+  //image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]
   @Input() persDetails;
   @Input() petsDetails;
+  form: FormGroup;
   formSubmitted: boolean = false;
+  imagePreview: string;
 
+  ngOnInit(){
+    this.form = new FormGroup({
+          image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]
+          })
+    });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+        this.imagePreview = (reader.result as string);
+    };
+    reader.readAsDataURL(file);
+  }
 
   submit(){
-    this.authService.createUser(this.regForm.get('personalDetails').get('firstname').value,
+    this.authService.createUser(
+    this.regForm.get('personalDetails').get('firstname').value,
     this.regForm.get('personalDetails').get('lastname').value,
     this.regForm.get('personalDetails').get('username').value,
     this.regForm.get('personalDetails').get('email').value,
-    this.regForm.get('personalDetails').get('password').value)
+    this.regForm.get('personalDetails').get('password').value,
+    this.form.value.image);
+    console.log(this.form.value.image);
+    
+    this.petService.createPet(
+      this.regForm.get('petDetails').get('petName').value,
+      this.regForm.get('petDetails').get('species').value,
+      this.regForm.get('petDetails').get('gender').value,
+      this.regForm.get('personalDetails').get('username').value,
+    );
   }
   // submit(): void{
   //   /*console.log('submitted');
