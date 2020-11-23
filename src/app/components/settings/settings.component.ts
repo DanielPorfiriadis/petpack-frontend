320f443abf5dc761bcbf73fe60a2b6f2fde8c85e
Gpapadoa <gpapadoa@gmail.com>
2020-11-23 17:47:59 +0200

    old settings

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -41,16 +41,14 @@ export class SettingsComponent implements OnInit, OnDestroy {
     this.userId = this.authService.getUserId();
     this.username = this.authService.getUserName();
     this.userNewDetails = new FormGroup({
-      'newFirstName': new FormControl(null, [Validators.required, Validators.minLength(2)]),
-      'newLastName': new FormControl(null, [Validators.required, Validators.minLength(2)]),
-      'newUsername': new FormControl(null, [Validators.required, Validators.minLength(4)]),
-      'newEmail': new FormControl(null, [Validators.required, Validators.email]),
-      'newPassword': new FormControl(null, [Validators.required, Validators.pattern('(?=.*[0-9])(?=.*[$@$!%*?&]).{6,}')]),
+      'newFirstName': new FormControl(null),
+      'newLastName': new FormControl(null),
+      'newUsername': new FormControl(null),
+      'newEmail': new FormControl(null),
+      'newPassword': new FormControl(null),
       'newAvatar': new FormControl(null),
-      'confirmPassword': new FormControl(null, [Validators.required]),
 
     })
-    
     this.petNewDetails = new FormGroup({
 
       'newPetName': new FormControl(null),
@@ -60,9 +58,9 @@ export class SettingsComponent implements OnInit, OnDestroy {
     })
     this.newPet = new FormGroup({
 
-      'newPetName': new FormControl(null, [Validators.required, Validators.pattern('^[A-Za-zñÑáéíóúÁÉÍÓÚ ]+$')]),
-      'newGender': new FormControl(null, Validators.required),
-      'newSpecies': new FormControl(null, Validators.required),
+      'newPetName': new FormControl(null),
+      'newGender': new FormControl(null),
+      'newSpecies': new FormControl(null),
       'newPetSpecies': new FormControl(null)
     })
     this.authService.getUserInfo(this.userId)
@@ -111,34 +109,20 @@ export class SettingsComponent implements OnInit, OnDestroy {
     this.iterations.push(this.x);
   }
 
-  petSaveSubmited() {
-
-    this.newPet.get('newPetName').markAsTouched();
-    this.newPet.get('newPetName').updateValueAndValidity();
-    this.newPet.get('newSpecies').markAsTouched();
-    this.newPet.get('newSpecies').updateValueAndValidity();
-    this.newPet.get('newGender').markAsTouched();
-    this.newPet.get('newGender').updateValueAndValidity();
-
-    if (!this.newPet.get('newPetName').hasError('required') &&
-      !this.newPet.get('newPetName').hasError('pattern') &&
-      !this.newPet.get('newSpecies').hasError('required') &&
-      !this.newPet.get('newGender').hasError('required'))
-    {
-      let pet: PetData = {
-        id: '',
-        petName: this.newPet.get('newPetName').value,
-        species: this.newPet.get('newSpecies').value,
-        gender: this.newPet.get('newGender').value,
-        ownerUsername: this.username
-        }
-      this.petService.createPet(pet.petName, pet.species, pet.gender, pet.ownerUsername)
-      this.router.routeReuseStrategy.shouldReuseRoute = function () {
-        return false;
-      }
-      this.router.onSameUrlNavigation = 'reload';
-      this.router.navigate(["/settings"]);
+  petSaveSubmited(){
+    let pet: PetData = {
+      id: '',
+      petName: this.petNewDetails.get('newPetName').value,
+      species: this.petNewDetails.get('newSpecies').value,
+      gender: this.petNewDetails.get('newGender').value,
+      ownerUsername: this.username
     }
+    this.petService.createPet(pet.petName, pet.species, pet.gender, pet.ownerUsername)
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
+      return false;
+    }
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate(["/settings"]);
   }
 
   petChangeSubmited(){
@@ -165,29 +149,17 @@ export class SettingsComponent implements OnInit, OnDestroy {
     reader.readAsDataURL(file);
   }
 
-  userSubmited() {
+  userSubmited(){
 
-    if (!this.userNewDetails.get('newFirstName').hasError('required') &&
-      !this.userNewDetails.get('newFirstName').hasError('minLength') &&
-      !this.userNewDetails.get('newLastName').hasError('required') &&
-      !this.userNewDetails.get('newLastName').hasError('minLength') &&
-      !this.userNewDetails.get('newUsername').hasError('required') &&
-      !this.userNewDetails.get('newUsername').hasError('minLength') &&
-      !this.userNewDetails.get('newEmail').hasError('required') &&
-      !this.userNewDetails.get('newEmail').hasError('email') &&
-      !this.userNewDetails.get('newPassword').hasError('required') &&
-      !this.userNewDetails.get('newPassword').hasError('pattern')) {
-      let image = this.userNewDetails.get('newAvatar').value;
-      this.authService.updateUser(
-        this.userNewDetails.get('newFirstName').value,
-        this.userNewDetails.get('newLastName').value,
-        this.userNewDetails.get('newUsername').value,
-        this.userNewDetails.get('newEmail').value,
-        this.userNewDetails.get('newPassword').value,
-        image
+     let image =this.userNewDetails.get('newAvatar').value;
+    this.authService.updateUser(
+      this.userNewDetails.get('newFirstName').value,
+      this.userNewDetails.get('newLastName').value,
+      this.userNewDetails.get('newUsername').value,
+      this.userNewDetails.get('newEmail').value,
+      this.userNewDetails.get('newPassword').value,
+      image
       );
-    }
-     
   }
 ngOnDestroy(){
   this.petSub.unsubscribe();
@@ -204,110 +176,17 @@ ngOnDestroy(){
     this.petArray.push(this.pets);
     console.log(this.petArray);
 
+/*    let _this = this;
+    this.service.SettingsService(this.newDetails).subscribe({
+      next: x => {
+        console.log(x);
+      },
+      error: error => {
+        _this.newDetails.hasError(error);
+      }
+    });*/
   }
 
-  matching() {
-    const pass = this.userNewDetails.get('newPassword').value;
-    const conf = this.userNewDetails.get('confirmPassword').value;
-    if (pass !== conf) {
-      return this.userNewDetails.get('confirmPassword').setErrors({ 'notMatching': true });
-    }
-    return this.userNewDetails.get('confirmPassword').setErrors(null);
-  }
 
-  getConfirmErrorMessage() {
-    if (this.userNewDetails.get('confirmPassword').hasError('notMatching')) {
-      return 'Passwords do not match'
-    }
-  }
-
-  /*email error message*/
-  getEmailErrorMessage() {
-    if (this.userNewDetails.get('newEmail').hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.userNewDetails.get('newEmail').hasError('email') ? 'Not a valid email' : '';
-  }
-
-  /*first name error message*/
-  getFirstnameErrorMessage() {
-    if (this.userNewDetails.get('newFirstName').hasError('required')) {
-      return 'You must enter a value';
-    }
-    return 'First name must be greater than 2 characters'
-
-  }
-
-  /*last name error message*/
-  getLastnameErrorMessage() {
-    if (this.userNewDetails.get('newLastName').hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return 'First name must be greater than 2 characters'
-
-  }
-
-  /*username error message*/
-  getUsernameErrorMessage() {
-    if (this.userNewDetails.get('newUsername').hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return 'First name must be greater than 4 characters'
-
-  }
-
-  /*password error message*/
-  getPasswordErrorMessage() {
-    if (this.userNewDetails.get('newPassword').hasError('required')) {
-      return 'You must enter a value';
-    }
-    return 'At least 6 characters and include: number(s) & special characters';
-  }
-
-  /*Pet Name Validation*/
-  getPetnameErrorMessage() {
-    if (this.petNewDetails.get('newPetName').hasError('required')) {
-      return 'You must enter a value';
-    }
-    return 'Only letters are allowed'
-
-  }
-
-  getSpeciesErrorMessage() {
-    if (this.petNewDetails.get('newSpecies').hasError('required')) {
-      return 'You must select a value';
-    }
-  }
-
-  getGenderErrorMessage() {
-    if (this.petNewDetails.get('newGender').hasError('required')) {
-      return 'You must select gender';
-    }
-  }
-
-/*New Pet*/
-
-  getNewPetnameErrorMessage() {
-    if (this.newPet.get('newPetName').hasError('required')) {
-      return 'You must enter a value';
-    }
-    return 'Only letters are allowed'
-
-  }
-
-  getNewSpeciesErrorMessage() {
-    if (this.newPet.get('newSpecies').hasError('required')) {
-      return 'You must select a value';
-    }
-  }
-
-  getNewGenderErrorMessage() {
-    if (this.newPet.get('newGender').hasError('required')) {
-      return 'You must select gender';
-    }
-  }
 }
 
